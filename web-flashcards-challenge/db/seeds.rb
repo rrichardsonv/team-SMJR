4c13bc4da791d305c137290b89650075e8a7b226
Robert Richardson & SaVance Ford <pair+rrichardsonv.savancex@devbootcamp.com>
2017-01-06 16:16:30 -0600

    Added to seed file

--- web-flashcards-challenge/db/seeds.rb
+++ web-flashcards-challenge/db/seeds.rb
@@ -9,3 +9,8 @@ Card.create(:question =>"Lightest wood",:answer => "balsa",:deck_id => 1)
 Card.create(:question =>"Ships masts were often",:answer => "Oak",:deck_id => 1)
 Card.create(:question =>"The most popular wood in Lebanon",:answer => "Cedar",:deck_id => 1)
 Card.create(:question =>"Pandas like me alot",:answer => "Bamboo",:deck_id =>1)
+
+rich_savance =[{:name => 'Rich', :email =>'rich@rich.com', :password=>'rich_password'},{:name=>'Savance', :email=>'savance@savance.com', :password=>'savance_password'}]
+
+User.create(rich_savance[1])
+User.create(rich_savance[0])
