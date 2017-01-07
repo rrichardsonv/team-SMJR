50f773ed5b9b8c7af6f45080951983d4bae384d5
Jim Hallberg & Michael  Taylor <pair+JHallberg5100.mikejtaylor@devbootcamp.com>
2017-01-07 10:57:27 -0600

    Making guess controllers.

--- web-flashcards-challenge/app/helpers/gamelogic.rb
+++ web-flashcards-challenge/app/helpers/gamelogic.rb
@@ -1,29 +1,31 @@
 helpers do
   def process_guess(args={})
-    current_card = Card.find(args[:guess][:card_id].to_i)
-    correct = args[:guess][:body] == current_card.answer
+
+    current_card = Card.find(args[:card_id].to_i)
+    #raise current_card.answer
+    #raise args[:body].inspect
+    correct = args[:body].downcase == current_card.answer.downcase
+
+
     guess_deets = { round_id: args[:round_id],
                   card_id: args[:card_id],
-                  body: args[:user_guess],
-                  correct: args[:correct] }
-    new_guess = Guess.new(guess_deets)
-
-    # if new_guess.save
-    #   raise 'saved now what!'
-    # else
-    #   @errors = new_guess.errors.full_messages
-    #   raise 'didnt save now what'
-    # end
+                  body: args[:body],
+                  correct: correct }
+    new_guess = Guess.create(guess_deets)
   end
+
   def end_of_deck
     completed_cards = @current_round.guesses.where(correct: true).cards
     all_cards = @current_round.cards
     all_cards.reject{|card| completed_cards.include?(card)}
   end
 
-  def done?(current_round)
-    completed_cards = current_round.guesses.where(correct: true).cards
-    all_cards = current_round.cards
+  def done?
+    #(current_round)
+    completed_cards = @current_round.guesses.select{|guess| guess[:correct]}.distinct
+    # raise completed_cards.first.inspect
+    #.where(correct: true)
+    all_cards = @current_round.cards
     if completed_cards.length == all_cards.length
       true
     else
--- web-flashcards-challenge/app/models/guess.rb
+++ web-flashcards-challenge/app/models/guess.rb
@@ -1,7 +1,9 @@
 class Guess < ActiveRecord::Base
   validates :round, presence: true
   validates :card, presence: true
-  validates :body, :correct, presence: true
+  validates :body, presence: true
+
+  validates :correct, inclusion: { in: [true, false] }
 
   belongs_to :card
   belongs_to :round
